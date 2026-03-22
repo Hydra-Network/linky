aecac3a22e190053d399b89c44083e20e47beec8
Coding4hours <138321129+Coding4Hours@users.noreply.github.com>
2026-03-22 15:55:46 -0700

    feat: invite command

---
+++ commands/utilities/invite.js
@@ -0,0 +1,26 @@
+import {
+	SlashCommandBuilder,
+	ApplicationIntegrationType,
+	InteractionContextType,
+} from "discord.js";
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName("invite")
+		.setDescription("Sends the Linky's invite link.")
+		.setIntegrationTypes([
+			ApplicationIntegrationType.GuildInstall,
+			ApplicationIntegrationType.UserInstall,
+		])
+		.setContexts([
+			InteractionContextType.Guild,
+			InteractionContextType.BotDM,
+			InteractionContextType.PrivateChannel,
+		]),
+	async execute(interaction) {
+		await interaction.reply({
+			content: "Invite: https://discord.com/oauth2/authorize?client_id=1469170337810743478",
+			flags: MessageFlags.Ephemeral,
+		});
+	}
+};
