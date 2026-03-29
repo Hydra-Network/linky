4e0ffeaf9596f00e9d8b0fc020225206c2bc01b2
Coding4hours <138321129+Coding4Hours@users.noreply.github.com>
2026-03-29 14:50:20 -0700

    fix: for prof

--- config/emojis.js
+++ config/emojis.js
@@ -1,7 +1,7 @@
 export const EMOJI_IDS = {
-	cat_attack: "1487927820888965353"
+	cat_attack: "1487927780938223657"
 }
 
 export const EMOJIS = {
-	cat_attack: "<:cat_attack:1487927820888965353>",
+	cat_attack: `<:cat_attack:${EMOJI_IDS.cat_attack}>`,
 };
