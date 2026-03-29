7bd481898899ed7aa2bd1fc5a874b5b2058ea93c
Coding4hours <138321129+Coding4Hours@users.noreply.github.com>
2026-03-29 14:17:33 -0700

    feat: there is a dev command with fancy logs now

--- utils/logger.js
+++ utils/logger.js
@@ -1,15 +1,19 @@
 import pino from "pino";
 
-const logger = pino({
-  transport: {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-      translateTime: "HH:MM:ss Z",
-      ignore: "pid,hostname",
-    },
-  },
-});
+const logger = pino(
+  process.env.NODE_ENV === "development"
+    ? {
+        transport: {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+            translateTime: "HH:MM:ss Z",
+            ignore: "pid,hostname",
+          },
+        },
+      }
+    : { level: "info" },
+);
 
 export function createChildLogger(context = {}) {
   return logger.child(context);
