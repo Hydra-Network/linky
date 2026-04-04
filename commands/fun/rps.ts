2852db4167c1d018a4e97aa0cf264f239d23fbf1
Coding4hours <138321129+Coding4Hours@users.noreply.github.com>
2026-04-04 11:20:45 -0700

    fix(rps): an import was only used as a type but wasnt type imported

--- commands/fun/rps.ts
+++ commands/fun/rps.ts
@@ -1,8 +1,9 @@
 import {
-	ChatInputCommandInteraction,
 	ApplicationIntegrationType,
+
+	type ChatInputCommandInteraction,
 	InteractionContextType,
-	SlashCommandBuilder
+	SlashCommandBuilder,
 } from "discord.js";
 import type { AppContainer } from "@/services/container.js";
 
@@ -11,19 +12,26 @@ export default {
 		.setName("rps")
 		.setDescription("Plays rock paper scissors with you.")
 		.addStringOption((o) =>
-			o.setName("choice")
+			o
+				.setName("choice")
 				.setDescription("Your move")
 				.addChoices(
 					{ name: "Rock", value: "Rock" },
 					{ name: "Paper", value: "Paper" },
-					{ name: "Scissors", value: "Scissors" }
+					{ name: "Scissors", value: "Scissors" },
 				)
-				.setRequired(true)
+				.setRequired(true),
 		)
 		.setIntegrationTypes([ApplicationIntegrationType.GuildInstall])
-		.setContexts([InteractionContextType.Guild, InteractionContextType.PrivateChannel]),
+		.setContexts([
+			InteractionContextType.Guild,
+			InteractionContextType.PrivateChannel,
+		]),
 
-	async execute(interaction: ChatInputCommandInteraction, _container: AppContainer) {
+	async execute(
+		interaction: ChatInputCommandInteraction,
+		_container: AppContainer,
+	) {
 		const userChoice = interaction.options.getString("choice", true);
 		const choices = ["Rock", "Paper", "Scissors"];
 		const botChoice = choices[Math.floor(Math.random() * choices.length)];
@@ -43,7 +51,7 @@ export default {
 		return interaction.reply(
 			userWon
 				? `You win! Your ${userChoice} beats my ${botChoice}.`
-				: `I win! My ${botChoice} beats your ${userChoice}.`
+				: `I win! My ${botChoice} beats your ${userChoice}.`,
 		);
 	},
 };
